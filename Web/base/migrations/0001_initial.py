17e0dcce2d9f7cf010818e05198f8e7a14857260
Irmak Sirer <irmak@northwestern.edu>
2014-01-11 23:13:13 -0600

    removed all migrations, recreated db from scratch, started with a fresh initial migration. Fixed the field problems

--- Web/base/migrations/0001_initial.py
+++ Web/base/migrations/0001_initial.py
@@ -10,17 +10,17 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding model 'Movie'
         db.create_table(u'base_movie', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('imdb_id', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('imdb_id', self.gf('django.db.models.fields.CharField')(max_length=255, primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=500)),
-            ('year', self.gf('django.db.models.fields.IntegerField')(default=0)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=1000)),
+            ('year', self.gf('django.db.models.fields.CharField')(default='N/A', max_length=100)),
+            ('director', self.gf('django.db.models.fields.CharField')(default='N/A', max_length=500)),
+            ('description', self.gf('django.db.models.fields.TextField')(default='N/A')),
             ('starRating', self.gf('django.db.models.fields.IntegerField')(default=0)),
             ('rawTrueSkillMu', self.gf('django.db.models.fields.FloatField')(default=3.0)),
             ('rawTrueSkillSigma', self.gf('django.db.models.fields.FloatField')(default=1.0)),
             ('starSeededTrueSkillMu', self.gf('django.db.models.fields.FloatField')(default=3.0)),
             ('starSeededTrueSkillSigma', self.gf('django.db.models.fields.FloatField')(default=1.0)),
-            ('poster_name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('poster_name', self.gf('django.db.models.fields.CharField')(default='_empty_poster.jpg', max_length=255)),
         ))
         db.send_create_signal(u'base', ['Movie'])
 
@@ -29,7 +29,7 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('movie1', self.gf('django.db.models.fields.related.ForeignKey')(related_name='movie_1', to=orm['base.Movie'])),
             ('movie2', self.gf('django.db.models.fields.related.ForeignKey')(related_name='movie_2', to=orm['base.Movie'])),
-            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 1, 9, 0, 0))),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 1, 11, 0, 0))),
             ('result', self.gf('django.db.models.fields.IntegerField')(default=-1)),
         ))
         db.send_create_signal(u'base', ['VersusMatch'])
@@ -58,17 +58,17 @@ class Migration(SchemaMigration):
     models = {
         u'base.movie': {
             'Meta': {'ordering': "['name', '-starRating']", 'object_name': 'Movie'},
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'imdb_id': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'description': ('django.db.models.fields.TextField', [], {'default': "'N/A'"}),
+            'director': ('django.db.models.fields.CharField', [], {'default': "'N/A'", 'max_length': '500'}),
+            'imdb_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
-            'poster_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'poster_name': ('django.db.models.fields.CharField', [], {'default': "'_empty_poster.jpg'", 'max_length': '255'}),
             'rawTrueSkillMu': ('django.db.models.fields.FloatField', [], {'default': '3.0'}),
             'rawTrueSkillSigma': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
             'starRating': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'starSeededTrueSkillMu': ('django.db.models.fields.FloatField', [], {'default': '3.0'}),
             'starSeededTrueSkillSigma': ('django.db.models.fields.FloatField', [], {'default': '1.0'}),
-            'year': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+            'year': ('django.db.models.fields.CharField', [], {'default': "'N/A'", 'max_length': '100'})
         },
         u'base.versusmatch': {
             'Meta': {'object_name': 'VersusMatch'},
@@ -77,7 +77,7 @@ class Migration(SchemaMigration):
             'movie1': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'movie_1'", 'to': u"orm['base.Movie']"}),
             'movie2': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'movie_2'", 'to': u"orm['base.Movie']"}),
             'result': ('django.db.models.fields.IntegerField', [], {'default': '-1'}),
-            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 1, 9, 0, 0)'})
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 1, 11, 0, 0)'})
         }
     }
 
