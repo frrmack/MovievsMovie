2a3be6cdaf7eb2bc65b520d966332c44e7319c66
Irmak Sirer <irmak@northwestern.edu>
2014-02-04 19:43:10 -0600

    correct error catching in the middleware now: only auth errors get an auth error message

--- Web/base/middleware.py
+++ Web/base/middleware.py
@@ -3,12 +3,15 @@ from social import exceptions as social_exceptions
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from base.views import error_page
 
+
 class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
-        if hasattr(social_exceptions, 'AuthCanceled'):
+
+        if hasattr(social_exceptions, exception.__class__.__name__):
             err_title = 'Authentication Error'
             err_msg = 'There was an error during authentication process.\n\n%s' % exception
             return error_page(request, err_title, err_msg)
+
         else:
             raise exception
 
